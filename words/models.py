8135e87e32266f1753064e247cf4da0c08591690
AKunshin <reyiby2015@gmail.com>
2023-05-30 11:40:00 +0300

    Create Word model, registered in admin

--- words/models.py
+++ words/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Word(models.Model):
+
+    GENDERS = [
+        ('mas', 'Masculine'),
+        ('fem', 'Feminine'),
+        ('neu', 'Neuter'),
+    ]
+
+    word = models.CharField(max_length=100, verbose_name="Word")
+    gender = models.CharField(
+        choices=GENDERS, max_length=10, verbose_name="Gender")
+
+    def __str__(self):
+        return self.gender + " " + self.word
