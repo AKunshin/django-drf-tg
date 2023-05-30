5ad15b0e8a61ba573490c56d8ef0db7a95e76f49
AKunshin <reyiby2015@gmail.com>
2023-05-30 14:36:09 +0300

    Created serilizers for a random word and the next word

--- words/views.py
+++ words/views.py
@@ -1,3 +1,31 @@
+import random
 from django.shortcuts import render
+from django.http import HttpResponseNotFound
+from rest_framework import serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Word
+
+
+class WordSerializator(serializers.ModelSerializer):
+    class Meta:
+        model = Word
+        fields = ['pk', 'gender', 'word']
+
+
+class RandomWord(APIView):
+    def get(self, *args, **kwargs):
+        all_words = Word.objects.all()
+        random_word = random.choice(all_words)
+        serialized_random_word = WordSerializator(random_word, many=False)
+        return Response(serialized_random_word.data)
+    
+
+class NextWord(APIView):
+    def get(self, request, pk, format=None):
+        word = Word.objects.filter(pk__gt=pk).first()
+        if not word:
+            return HttpResponseNotFound()
+        serialized_word = WordSerializator(word, many=False)
+        return Response(serialized_word.data)
